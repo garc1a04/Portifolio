849f386c7e9ea029c6e73d9041167df2cb74b9bd
Guilherme <guigarciamonteiro@gmail.com>
2025-06-16 17:24:12 -0300

    V1.0.0 Finalizado

--- Script/main.js
+++ Script/main.js
@@ -1,3 +1,25 @@
+let navbar = document.querySelector(".fixed");
+let sections = document.querySelectorAll("section");
+
+let navLinks = document.querySelectorAll("header div nav ul");
+
+let altura = window.scrollY;
+if(altura != 0) {
+    navbar.classList.add("fixed_list");
+}
+
+window.onscroll = () => {
+    let altura = window.scrollY;
+
+    if(altura == 0) {
+        navbar.classList.remove("fixed_list");
+        return;
+    }
+
+    navbar.classList.add("fixed_list")
+};
+
+
 new JParticles.Particle('#demo', {
     range: 1.5,
     num: 0.25,
@@ -7,4 +29,3 @@ new JParticles.Particle('#demo', {
     maxR: 1.5,
     color: ['#FFFFFF', '#7A00FF','#FF0057','#FF00FF']
 });
-
