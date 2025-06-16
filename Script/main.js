673e49576de0fe569336fd772004b539edc32ba5
Guilherme <guigarciamonteiro@gmail.com>
2025-06-16 12:15:03 -0300

    Update in Particles, Animation and Texts

--- Script/main.js
+++ Script/main.js
@@ -1,10 +1,10 @@
 new JParticles.Particle('#demo', {
-    range: 1,
-    num: 0.2,
+    range: 1.5,
+    num: 0.25,
     minSpeed: 0.1,
     maxSpeed: 0.3,
-    minR: 1.2,
-    maxR: 1.2,
-    color: ['#FFFFFF']
+    minR: 1.3,
+    maxR: 1.5,
+    color: ['#FFFFFF', '#7A00FF','#FF0057','#FF00FF']
 });
 
